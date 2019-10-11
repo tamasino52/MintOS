e448153a6f495ba777a12fc2a0259342f59b13c0
tamasino52 <tkdzma8080@naver.com>
2019-10-11 13:56:51 +0900

    Update BootLoader.asm

--- os/00.BootLoader/BootLoader.asm
+++ os/00.BootLoader/BootLoader.asm
@@ -164,6 +164,7 @@ READEND:
 	mov dx, 534D4150h
 	mov cx, 20
 	int 15h
+	add cx, 30h
 	push cx							; 출력할 메시지의 어드레스를 스택에 삽입
     push 3                          ; 화면 Y 좌표(1)를 스택에 삽입
     push 0							; 화면 X 좌표(20)를 스택에 삽입
